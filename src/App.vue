74633e45e1fbd99305c94c3f7b2c87ff77af2289
azeezabidoye <azeezabidoye@yahoo.com>
2020-10-22 04:59:50 +0100

    First Deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <template>
+  <div id="app" :class="typeof weathers.main != 'undefined' && weathers.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="searchBox">
+        <input type="text" 
+        class="searchBar" 
+        placeholder="Search..."
+        v-model="query"
+        @keypress="fetchWeather" 
+        />
+      </div>
+
+      <div class="weatherWrap" v-if="typeof weathers.main != 'undefined'">
+        <div class="locationBox">
+          <div class="location">{{ weathers.name }}, {{ weathers.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+      </div>
+      
+
+      <div class="weatherBox">
+        <div class="temp">{{ Math.round(weathers.main.temp) }}<sup>o</sup>C</div>
+        <div class="weatherStatus">{{ weathers.weather[0].main }}</div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      api_key: 'd573a0ecb765039db0fe03c2cfd09aa0', 
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weathers: {}
+    }
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == 'Enter') {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weathers = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+      let date = d.getDate();
+      let day = days[d.getDay()];
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
